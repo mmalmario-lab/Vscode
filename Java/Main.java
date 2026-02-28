f2975dacb08875b45b5c67141282e52394017371
Tino <mmalmario@ccc.edu.ph>
2026-02-28 12:28:56 +0800

    add class to main java

--- Java/Main.java
+++ Java/Main.java
@@ -1,3 +1,10 @@
-public static void main(String[] args) {
-    System.out.println("Hello, World!");
+public class Main {
+    int x = 5;
+
+    public static void main(String[] args) {
+        Main myObj1 = new Main();
+        Main myObj2 = new Main();
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+    }
 }
